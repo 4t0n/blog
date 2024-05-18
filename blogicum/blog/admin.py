b354cc34ccda983eed6d79ecf8586e36612c7874
Vladimir Shvab <shvab-vladimir@yandex.ru>
2024-05-18 18:40:23 +0300

    for second review

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from django.db import models
 
-from .models import Category, Location, Post
+from .models import Category, Location, Post, Comment
 
 
-@admin.register(Category, Location, Post)
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Category, Location, Comment)
+class PostInfoAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'comments_count')
+
+    @admin.display(description='Количество комментариев')
+    def comments_count(self, obj):
+        return obj.comments.count()
